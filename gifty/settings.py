5eb6fd7325568dfa0032cce01ce1643f2425f87c
Juan Benito Pacheco <juanbenito.pr@gmail.com>
2016-03-31 09:21:43 +0200

    Add style in detail and profile_other pages

--- gifty/settings.py
+++ gifty/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+2'
 
 USE_I18N = True
 
